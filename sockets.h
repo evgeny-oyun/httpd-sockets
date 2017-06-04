16f2daa234df7c037c780e34b0438021953a6209
Evgeny Oyun <evgeny.oyun@gmail.com>
2017-06-04 11:25:50 +0300

    Update sockets.h

--- sockets.h
+++ sockets.h
@@ -1,5 +1,5 @@
-#ifndef _HTTP_SOCKETS_H
-#define _HTTP_SOCKETS_H
+#ifndef _SOCKETS_H
+#define _SOCKETS_H
 
 /* headers */
 #include <stdio.h>
@@ -25,25 +25,25 @@
 #include <ctype.h>
 
 /* structures */
-typedef struct httpd_socket_s{
+typedef struct socket_s{
   int backlog;
   int timeout;
   int buffer_size;
   int socket;
-} httpd_socket_t;
+} socket_t;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-extern void httpd_socket_free(httpd_socket_t*);
-extern httpd_socket_t*httpd_socket_init(const char*, int, int, int);
-extern int httpd_socket_tcp(httpd_socket_t*, const char*, const char*);
-extern int httpd_socket_unix(httpd_socket_t *st, const char *path);
-extern void httpd_socket_set_nonblock(int);
+extern void socket_free(socket_t*);
+extern socket_t *socket_init(const char*, int, int, int);
+extern int socket_tcp(socket_t*, const char*, const char*);
+extern int socket_unix(socket_t *st, const char *path);
+extern void socket_set_nonblock(int);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // _HTTP_SOCKETS_H
+#endif // _SOCKETS_H
